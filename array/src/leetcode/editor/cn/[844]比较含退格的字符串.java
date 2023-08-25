2c88658b72b6a776022af6a049822096dc132710
funzza <m784864336@outlook.com>
2023-08-25 17:03:44 +0800

    比较含退格的字符串

---
+++ array/src/leetcode/editor/cn/[844]比较含退格的字符串.java
@@ -0,0 +1,123 @@
+//给定 s 和 t 两个字符串，当它们分别被输入到空白的文本编辑器后，如果两者相等，返回 true 。# 代表退格字符。 
+//
+// 注意：如果对空文本输入退格字符，文本继续为空。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "ab#c", t = "ad#c"
+//输出：true
+//解释：s 和 t 都会变成 "ac"。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "ab##", t = "c#d#"
+//输出：true
+//解释：s 和 t 都会变成 ""。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "a#c", t = "b"
+//输出：false
+//解释：s 会变成 "c"，但 t 仍然是 "b"。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length, t.length <= 200 
+// s 和 t 只含有小写字母以及字符 '#' 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 你可以用 O(n) 的时间复杂度和 O(1) 的空间复杂度解决该问题吗？ 
+// 
+//
+// Related Topics 栈 双指针 字符串 模拟 👍 658 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public  boolean backspaceCompare(String s, String t) {
+
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        int skipS = 0;
+        int skipT = 0;
+
+        while ( i >= 0 && j >= 0) {
+
+            char sChar = i >= 0 ? s.charAt(i) : ' ';
+            char tChar = j >= 0 ? t.charAt(j) : ' ';
+
+            while (sChar == '#' || skipS > 0) {
+                if (sChar == '#') {
+                    skipS++;
+                } else {
+                    skipS--;
+                }
+                i--;
+                sChar = i >= 0 ? s.charAt(i) : ' ';
+            }
+
+            while (tChar == '#' || skipT > 0) {
+                if (tChar == '#') {
+                    skipT++;
+                } else {
+                    skipT--;
+                }
+                j--;
+                tChar = j >= 0 ? t.charAt(j) : ' ';
+            }
+
+            if (sChar != tChar) {
+                return false;
+            }
+            i--;
+            j--;
+        }
+
+        if (i >= 0) {
+            while (i >= 0 && (s.charAt(i) == '#' || skipS > 0)) {
+                if (s.charAt(i) == '#') {
+                    skipS++;
+                } else {
+                    skipS--;
+                }
+                i--;
+            }
+            if (i >= 0) {
+                return false;
+            }
+        }
+
+        if (j >= 0) {
+            while (j >= 0 && (t.charAt(j) == '#' || skipT > 0)) {
+                if (t.charAt(j) == '#') {
+                    skipT++;
+                } else {
+                    skipT--;
+                }
+                j--;
+            }
+            if (j >= 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
